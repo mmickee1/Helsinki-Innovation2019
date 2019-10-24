de3c34a1010ff47dfdcb58bb684c1f91993ee698
Mikael M <mikael_96@live.com>
2019-10-24 17:57:11 +0300

    updated middleviews graphs to be circle images

--- screens/GeneralGraphScreen.vue
+++ screens/GeneralGraphScreen.vue
@@ -1,9 +1,73 @@
 <template>
-  <view class="container">
-    <text class="heading">All graphs</text>
-    <text>App General Graphs screen</text>
+  <view>
+    <scroll-view>
+      <view class="container">
+        <text class="heading">All graphs</text>
+      </view>
+      <view class="wrapper">
+        <view class="leftside" id="btn">
+          <!--  <button v-bind:title="messageDetailed" v-bind:on-press="nextPage"></button>-->
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+        </view>
+
+        <view class="rightside" id="btn">
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+
+          <touchable-opacity :on-press="onPressButton">
+            <image :style="{width: width, height: height}" :source="{uri: imagecircle}" />
+          </touchable-opacity>
+        </view>
+      </view>
+    </scroll-view>
   </view>
 </template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  data: function() {
+    return {
+      messageDetailed: "Go to DetailedGraphScreen screen",
+      imagecircle:
+        "https://latex.artofproblemsolving.com/a/e/e/aee55bc00dcef6c78990e2d1dfd119492d1cce7f.png",
+      height: 150,
+      width: 150
+    };
+  },
+  methods: {
+    nextPage() {
+      alert("Hello");
+      //this.navigation.navigate("GeneralGraph");
+    },
+    goToDetailedGraphScreen() {
+      this.navigation.navigate("DetailedGraph");
+    },
+    onPressButton() {
+      this.navigation.navigate("DetailedGraph");
+    }
+  }
+};
+</script>
 
 <style>
 .container {
@@ -16,5 +80,26 @@
   font-weight: bold;
   color: darkolivegreen;
   margin: 20px;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
 }
+.rightside {
+  align-items: center;
+  background-color: aqua;
+  float: right;
+  width: 50%;
+}
+.leftside {
+  align-items: center;
+  background-color: gold;
+  float: left;
+  width: 50%;
+}
+/*#btn {
+  margin-left: 100px;
+  margin-right: 100px;
+  padding-right: 60px;
+  padding-left: 10px;
+}*/
 </style>
